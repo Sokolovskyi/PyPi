d2bb37ea99dc6ac715550a09113f4b32681d35e7
Sokolovskyi <Sokolovskyi@wks58.hirata.local>
2018-04-09 16:58:21 +0200

    stepper

--- ws/myServer.py
+++ ws/myServer.py
@@ -5,8 +5,10 @@ import datetime
 import random
 
 import websockets
+import json
 
 import servo.servoblaster
+#from servo import stepperMotor
 
 
 servoChannel = 2
@@ -15,8 +17,19 @@ async def time(websocket, path):
     while True:
         val = await websocket.recv()
         print("Try to go {}".format(val))
+        axis = json.loads(val)
+        for axe in axis:        # Second Example
+            print ('Current axe :', axe, ' ', axis[axe])
         
-        #servo.servoblaster.setServo(servoChannel, val)
+        if 'axe1' in axis.keys():
+            print ('Axe 1 will be controled with value ', axis['axe1'])
+            #stepperMotor.forward(0.1, axis['axe1'])
+        if 'axe2' in axis.keys():
+            print ('Axe 2 will be controled with value ', axis['axe2'])
+            #servo.servoblaster.setServo(servoChannel, val)
+        if 'axe3' in axis.keys():
+            print ('Axe 3 will be controled with value ', axis['axe3'])
+            #servo.servoblaster.setServo(servoChannel, val)
         
         greeting  = "Position {} is reached at ".format(val) + datetime.datetime.utcnow().isoformat() + 'Z'
         await websocket.send(greeting )
